836c7bb9e1ec7d5b196c64addb0273fb6baa6bd4
Tianon Gravi <admwiggin@gmail.com>
2014-04-02 17:25:54 -0600

    Preserve trailing slash even for relative paths
    Docker-DCO-1.1-Signed-off-by: Andrew Page <admwiggin@gmail.com> (github: tianon)

--- server/buildfile.go
+++ server/buildfile.go
@@ -487,7 +487,12 @@ func (b *buildFile) CmdAdd(args string) error {
 	}
 
 	if !filepath.IsAbs(dest) {
-		dest = filepath.Join("/", b.config.WorkingDir, dest)
+		newDest := filepath.Join("/", b.config.WorkingDir, dest)
+		if strings.HasSuffix(dest, "/") {
+			// preserve trailing slash
+			newDest += "/"
+		}
+		dest = newDest
 	}
 
 	cmd := b.config.Cmd
